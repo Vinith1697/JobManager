3a6284b1e0a4db0cd0da30c3c5f368bf2b072856
Vinith1697 <97vinith97@gmail.com>
2022-05-29 19:54:46 -0400

    Second commit: Cleared all the previous files and added a new landing page as WelcomePage saying Welcome to Job Manager. Also titled the flyout as Welcome

---
+++ JobManager/JobManager/AppShell.xaml.cs
@@ -0,0 +1,18 @@
+﻿
+using JobManager.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace JobManager
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            Routing.RegisterRoute(nameof(WelcomePage), typeof(WelcomePage));
+        }
+    }
+}
---
+++ xamarin-101-master/xamarin-101-master/xaml-ui/XAMLUI/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace XAMLUI
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected void SaveButton_Clicked(object sender, EventArgs e)
+        {
+            var noteText = noteEditor.Text;
+
+            noteEditor.Text = string.Empty;
+
+            textLabel.Text = noteText;
+        }
+
+        protected void DeleteButton_Clicked(object sender, EventArgs e)
+        {
+            noteEditor.Text = string.Empty;
+            textLabel.Text = string.Empty;
+        }
+    }
+}
